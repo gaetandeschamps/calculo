e2dd6c2519e83d131fe152852665eff87e44b82c
qlejour <quentin.lejour@hei.yncrea.fr>
2018-09-06 08:56:33 +0200

    test

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.css']
+})
+export class AccueilComponent implements OnInit {
+
+  utilisateur: String = 'thomas';
+  pokemon: String = 'pikachu';
+  show: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  showPokemon() {
+    this.show = !this.show;
+  }
+
+}
