4d7c505a7e53c2d660ab82fe91bc96cb0500aa1e
qlejour <quentin.lejour@hei.yncrea.fr>
2018-09-18 16:57:59 +0200

    debut creation calcul composant

--- src/app/calcul.service.ts
+++ src/app/calcul.service.ts
@@ -14,4 +14,22 @@ export class CalculService {
     return randomNumbers;
   }
 
+  randomNumber(range: number) {
+    return Math.floor((Math.random() * range) + 1);
+  }
+
+  setAddition(range: number) {
+    const firstNumber: number = this.randomNumber(range);
+    const secondNumber: number = this.randomNumber(range);
+    const answer: number = firstNumber + secondNumber;
+    const calculParameters = {
+      'firstNumber': firstNumber,
+      'secondNumber': secondNumber,
+      'answer': answer,
+      'title': 'Résous l\'addition',
+      'operator': '+'
+    }
+    return calculParameters;
+  }
+
 }
--- src/app/calcul/calcul.component.ts
+++ src/app/calcul/calcul.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { CalculService } from '../../calcul.service';
+import { CalculService } from '../calcul.service';
 
 @Component({
-  selector: 'app-additions',
-  templateUrl: './additions.component.html',
-  styleUrls: ['./additions.component.css']
+  selector: 'app-calcul',
+  templateUrl: './calcul.component.html'
 })
-export class AdditionsComponent implements OnInit {
+export class CalculComponent implements OnInit {
 
-  private randomNumbers: number[];
-  private answer: number;
+  private parameters;
   private userAnswer: number;
   private answerCheck: boolean;
   private nbredeVie = 2;
@@ -24,7 +22,7 @@ export class AdditionsComponent implements OnInit {
   }
 
   additionAnswer() {
-    if (this.userAnswer === this.answer) {
+    if (this.userAnswer === this.parameters.answer) {
       this.answerCheck = true;
     } else {
       this.answerCheck = false;
@@ -38,9 +36,9 @@ export class AdditionsComponent implements OnInit {
     this.nbredeVie = 2;
     this.userAnswer = null;
     this.answerCheck = null;
-    this.randomNumbers = this.calculService.generateRandomNumbers(15);
-    this.answer = this.randomNumbers[0] + this.randomNumbers[1];
     this.button = 'Valider';
+    this.parameters = this.calculService.setAddition(15);
+    console.log(this.parameters);
   }
 
 }
