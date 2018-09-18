434e99508b6ef9083ac2466ecc98e801da826416
Gaëtan Deschamps <gaetandeschamps@macbook-pro-de-gaetan-2.home>
2018-09-18 21:54:16 +0200

    ajout bouton 'afficher les résultats'

--- src/app/calcul/calcul.component.ts
+++ src/app/calcul/calcul.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Injectable } from '@angular/core';
 import { CalculService } from '../calcul.service';
 
 @Component({
   selector: 'app-calcul',
   templateUrl: './calcul.component.html'
 })
+
 export class CalculComponent implements OnInit {
 
   @Output() nextQuestion = new EventEmitter<boolean>();
-
+  
   private parameters;
   private userAnswer: number;
   private answerCheck: boolean;
-  private nbredeVie = 2;
-  private button: String;
-
+  private nbredeVie: number=1;
+  private boutonValider: String;
+  private boutonCalculSuivant: String;
+  private nbQuestion: number=1;
+  
   constructor(
     private calculService: CalculService
   ) { }
@@ -29,24 +32,32 @@ export class CalculComponent implements OnInit {
     } else {
       this.answerCheck = false;
       this.nbredeVie--;
+      this.boutonValider = 'Retenter ma chance !'; 
     }
-    this.userAnswer = null;
-    this.button = 'Retenter ma chance !';
+    if(this.nbQuestion === 10){
+      this.boutonCalculSuivant = "Afficher les résultats";
+    }
   }
 
   setCalcul() {
-
     if (this.answerCheck === true) {
       this.nextQuestion.emit(true);
     } else if (this.answerCheck === false) {
       this.nextQuestion.emit(false);
     }
-
     this.nbredeVie = 2;
     this.userAnswer = null;
     this.answerCheck = null;
-    this.button = 'Valider';
+    this.boutonValider = 'Valider';
+    this.boutonCalculSuivant = 'Calcul Suivant';
     this.parameters = this.calculService.setAddition(15);
+    console.log(this.parameters);    
   }
+
+  nombreQuestion(){
+    this.nbQuestion++;
+    return this.nbQuestion;
+  }
+
 
 }
